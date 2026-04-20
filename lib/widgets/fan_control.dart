8b1320db245597402337ca0557c791e0c2174e18
Kollias <71561676+KolliasG7@users.noreply.github.com>
2026-04-21 02:51:56 +0300

    Update fan_control.dart

--- lib/widgets/fan_control.dart
+++ lib/widgets/fan_control.dart
@@ -12,7 +12,7 @@ class FanControlCard extends StatefulWidget {
 
 class _FanControlCardState extends State<FanControlCard>
     with SingleTickerProviderStateMixin {
-  static const _min = 20.0, _max = 85.0;
+  static const _min = 30.0, _max = 85.0;
   late double _val;
   bool _writing = false;
   int? _confirmed;
@@ -99,7 +99,7 @@ class _FanControlCardState extends State<FanControlCard>
             overlayShape: const RoundSliderOverlayShape(overlayRadius: 18),
           ),
           child: Slider(
-            value: _val, min: _min, max: _max, divisions: 65,
+            value: _val, min: _min, max: _max, divisions: 55,
             onChanged: _writing ? null : (v) => setState(() => _val = v),
             onChangeEnd: _writing ? null : _commit,
           ),
